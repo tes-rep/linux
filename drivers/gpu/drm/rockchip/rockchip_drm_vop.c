6146aa86c7a35fec1c8cdcfbdeeb097090411c53
Jonas Karlman <jonas@kwiboo.se>
2020-07-20 15:15:50 +0000

    WIP/1000: drm/rockchip: vop: filter interlaced modes
    The current version of the driver does not support interlaced modes,
    lets filter any interlaced mode.
    
    Signed-off-by: Jonas Karlman <jonas@kwiboo.se>

--- drivers/gpu/drm/rockchip/rockchip_drm_vop.c
+++ drivers/gpu/drm/rockchip/rockchip_drm_vop.c
@@ -1203,6 +1203,9 @@ static enum drm_mode_status vop_crtc_mode_valid(struct drm_crtc *crtc,
 	if (vop->data->max_output.width && mode->hdisplay > vop->data->max_output.width)
 		return MODE_BAD_HVALUE;
 
+	if (mode->flags & DRM_MODE_FLAG_INTERLACE)
+		return MODE_NO_INTERLACE;
+
 	return MODE_OK;
 }
 
