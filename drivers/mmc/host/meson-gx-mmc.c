8e20e71687cbff420496cd55407fb7871ec5011a
devmfc <devmfc@mvand.net>
2023-10-03 16:59:29 +0200

    [DEVMFC] mmc: meson-gx: Add support for Meson S4

--- drivers/mmc/host/meson-gx-mmc.c
+++ drivers/mmc/host/meson-gx-mmc.c
@@ -456,6 +456,11 @@ static int meson_mmc_clk_init(struct meson_host *host)
 			clk_set_rate(clk, 24000000);
 
 		mux_parent_names[i] = __clk_get_name(clk);
+		
+		if(i==0)
+			ret = clk_set_rate(clk, 24000000);  // DEVMFC: from 4.9.180 needed for SC2 and S4
+		if (ret) 
+			return ret;
 	}
 
 	/* create the mux */
@@ -1343,6 +1348,7 @@ static const struct of_device_id meson_mmc_of_match[] = {
 	{ .compatible = "amlogic,meson-gxl-mmc",	.data = &meson_gx_data },
 	{ .compatible = "amlogic,meson-gxm-mmc",	.data = &meson_gx_data },
 	{ .compatible = "amlogic,meson-axg-mmc",	.data = &meson_axg_data },
+	{ .compatible = "amlogic,meson-s4-mmc",		.data = &meson_axg_data },
 	{}
 };
 MODULE_DEVICE_TABLE(of, meson_mmc_of_match);
