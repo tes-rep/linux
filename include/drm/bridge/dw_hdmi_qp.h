4b07089a5203c44747e15e880e912cd0c2b3cb96
Cristian Ciocaltea <cristian.ciocaltea@collabora.com>
2025-07-03 12:42:38 +0300

    [WIP-FRL] drm/bridge: dw-hdmi-qp: Add HDMI 2.1 FRL support
    Implement the link training state machine required to support HDMI 2.1
    FRL display modes.
    
    This has been verified up to 4K@160Hz, although the actual refresh rate
    (as indicated by the display) seems to not exceed 130Hz.  Note the
    RK3588 TRM only mentions the 4K@120Hz and 8K@60Hz modes as being
    supported by the HDMI TX Controller, hence it's not entirely clear what
    is the actual hardware limitation.
    
    Signed-off-by: Cristian Ciocaltea <cristian.ciocaltea@collabora.com>

--- include/drm/bridge/dw_hdmi_qp.h
+++ include/drm/bridge/dw_hdmi_qp.h
@@ -7,16 +7,27 @@
 #ifndef __DW_HDMI_QP__
 #define __DW_HDMI_QP__
 
+#include <linux/types.h>
+
 struct device;
 struct drm_encoder;
 struct dw_hdmi_qp;
 struct platform_device;
+
+struct dw_hdmi_qp_link_config {
+	bool frl_enabled;
+	u8 frl_rate_forced;
+	u8 frl_rate_per_lane;
+	u8 frl_lanes;
+};
 
 struct dw_hdmi_qp_phy_ops {
 	int (*init)(struct dw_hdmi_qp *hdmi, void *data);
 	void (*disable)(struct dw_hdmi_qp *hdmi, void *data);
 	enum drm_connector_status (*read_hpd)(struct dw_hdmi_qp *hdmi, void *data);
 	void (*setup_hpd)(struct dw_hdmi_qp *hdmi, void *data);
+	struct dw_hdmi_qp_link_config *(*get_link_cfg)(struct dw_hdmi_qp *hdmi, void *data);
+	int (*force_link_rate)(struct dw_hdmi_qp *hdmi, void *data, u8 frl_rate);
 };
 
 struct dw_hdmi_qp_plat_data {
